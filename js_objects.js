e6e78e58b1c10b43ff03414b2f71906a5ddc7d09
Brandon S Navone <bn03907@srcs.loc>
2018-01-18 12:05:07 -0800

    first save

---
+++ js_objects.js
@@ -0,0 +1,58 @@
+function Cube(length, mass) {
+    this.length = length;
+    this.mass = mass;
+    this.volume = Math.pow(length, 3);
+    this.density = this.mass / this.volume;
+    this.surfaceArea = Math.pow(length, 2) * length * 2;
+
+}
+
+
+//----------------------------------------------------------//
+
+function Person(firstName, lastName, bday) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.bday = bday;
+    this.sayHi = sayHi;
+    this.age = age;
+    this.getSign = getSign;
+}
+
+function sayHi() {
+    var today = new Date();
+    var hour = today.getHours();
+    var day = today.getDay();
+    if (hour < 10) {
+        if (day == 0 || day == 6) {
+            return "Go back to sleep! Its the weekend!";
+        } else if (day == 1) {
+            return "Ugh, its a Monday monring!";
+        } else {
+            return "Good Morning!";
+        }
+    } else if (hour < 18) {
+        if (day == 0 || day == 6) {
+            return "Hope you are having a great weekend!";
+        } else {
+            return "Good Day!";
+        }
+    } else {
+        if (day == 0) {
+            return "Enjoy your evening...tomorrow is Monday!"
+        } else {
+            return "Good Evening!";
+        }
+    }
+}
+
+function age() {
+    var today = new Date();
+    var birth = new Date(this.bday);
+    var years = Math.floor((today.getTime() - birth.getTime()) / 31536000000);
+    return years;
+}
+
+function getSign() {
+
+}
