73aa0008409934da0fb9110a40cae7b62529f439
Brandon S Navone <bn03907@srcs.loc>
2018-01-18 12:19:01 -0800

    fea

--- js_objects.js
+++ js_objects.js
@@ -53,6 +53,8 @@ function age() {
     return years;
 }
 
-function getSign() {
-
+function getSign(day, month) {
+    var zodiac = ['', 'Capricorn', 'Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn'];
+    var last_day = ['', 19, 18, 20, 20, 21, 21, 22, 22, 21, 22, 21, 20, 19];
+    return (day > last_day[month]) ? zodiac[month * 1 + 1] : zodiac[month];
 }
